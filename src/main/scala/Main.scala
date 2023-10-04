44ada554e86ce39660dcb1c84c7c93326a4e97d7
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-10-05 00:05:52 +0200

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,12 +9,24 @@ import scala.concurrent.duration.Duration
 
 object HelloWorld extends App {
 
-  implicit val system = ActorSystem("ProxySystem")
-  implicit val mat = ActorMaterializer()
+  implicit val system: ActorSystem = ActorSystem("ProxySystem")
+  implicit val mat: ActorMaterializer = ActorMaterializer()
 
   val route: Route = get {
     pathSingleSlash {
-      complete("Hello World")
+      // https://stackoverflow.com/a/68588177/594538
+      import java.io.FileInputStream
+      import java.util.jar.JarInputStream
+
+      val scala3LibJar = classOf[CanEqual[_, _]].getProtectionDomain.getCodeSource.getLocation.toURI.getPath
+      val manifest = new JarInputStream(new FileInputStream(scala3LibJar)).getManifest
+      val runtimeVersion = manifest.getMainAttributes.getValue("Implementation-Version")
+
+      complete(
+        "Hello World\n" +
+        "scala runtime: " + runtimeVersion + "\n" +
+        "scala stdlib: " + scala.util.Properties.versionNumberString + "\n" +
+        "java: " + scala.util.Properties.javaVersion)
     }
   }
   val port = (scala.util.Properties.envOrElse("PORT", "8080")).toInt
